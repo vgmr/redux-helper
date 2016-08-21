6f82035c44329b9394c6773b7828e16226a86d34
Daniele Giove <jodan@JoMcFly.station>
2016-08-21 17:15:58 +0200

    df

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, createCheckedAction, CreateAction, CheckedActionOptions, Action } from './actionCreators';
-import checkedPromiseMiddleware, { CheckedPromiseMiddlewareOptions } from './checkedPromiseMiddleware';
-export default checkedPromiseMiddleware;
-export { Action, checkedPromiseMiddleware, CheckedPromiseMiddlewareOptions, createAction, createCheckedAction, CreateAction, CheckedActionOptions };
