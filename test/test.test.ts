87f6829d0a61f05737ef2f36187d2527062a4930
Jd <jodan.pz@gmail.com>
2017-04-10 18:53:42 +0200

    Update test.test.ts

--- test/test.test.ts
+++ test/test.test.ts
@@ -1,3 +1,25 @@
+// MIT License
+
+// Copyright (c) 2016-17 (vgmr)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import * as mocha from 'mocha';
 import * as  expect from 'expect';
 import * as lib from '../src';
@@ -24,22 +46,20 @@ describe('Action Creators', () => {
         expect(simpleAction.matchAction(act2)).toEqual(false);
     })
 
-    it('createAction.typeCreated', () => {
+    it('createAction.type', () => {
         const simpleAction = lib.createAction<string>('TEST_ACTION');
-
-        expect(simpleAction.typeCreated).toEqual('TEST_ACTION');
+        expect(simpleAction.type).toEqual('TEST_ACTION');
     })
 
-    it('createPromiseAction.typeCreated', () => {
+    it('createPromiseAction.type', () => {
         const resultAction = lib.createAction<string>('RESULT_ACTION');
         const promiseAction = lib.createPromiseAction<string, string>(
             'TEST_PROMISE_ACTION',
             (value) => Promise.resolve('value'),
             resultAction);
 
-        expect(promiseAction.typeCreated).toEqual('TEST_PROMISE_ACTION');
+        expect(promiseAction.type).toEqual('TEST_PROMISE_ACTION');
     })
-
 });
 
 
